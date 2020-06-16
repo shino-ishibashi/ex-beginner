148831837a916f01b6d6bb3d025199363e51d190
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-06-16 11:05:42 +0900

    create form class

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,4 +1,28 @@
 package com.example.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletContext;
+
+@Controller
+@RequestMapping("/ex03")
 public class Exam03Controller {
+
+    @Autowired
+    private ServletContext application;
+
+    @RequestMapping("")
+    public String index(){
+        return "exam03";
+    }
+
+    @RequestMapping("sum-price")
+    public String sumPrice(){
+
+
+        return
+    }
 }
---
+++ src/main/java/com/example/form/Exam03Form.java
@@ -0,0 +1,33 @@
+package com.example.form;
+
+public class Exam03Form {
+    private Integer product1Price;
+    private Integer product2Price;
+    private Integer product3Price;
+
+
+//    getter setter
+    public Integer getProduct1Price() {
+        return product1Price;
+    }
+
+    public void setProduct1Price(Integer product1Price) {
+        this.product1Price = product1Price;
+    }
+
+    public Integer getProduct2Price() {
+        return product2Price;
+    }
+
+    public void setProduct2Price(Integer product2Price) {
+        this.product2Price = product2Price;
+    }
+
+    public Integer getProduct3Price() {
+        return product3Price;
+    }
+
+    public void setProduct3Price(Integer product3Price) {
+        this.product3Price = product3Price;
+    }
+}
