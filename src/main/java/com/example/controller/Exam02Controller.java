9fc891e19c9c34055e975d6b47dd427d4dcbb4cf
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-06-16 10:40:58 +0900

    controllerの実装

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,4 +1,21 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex02")
 public class Exam02Controller {
+
+
+    /**
+     * 入力画面にフォワードするメソッド
+     * @return 入力画面のhtmlのパス
+     */
+    @RequestMapping("")
+    public String index(){
+        return "/exam02/exam02";
+    }
+
+
 }
